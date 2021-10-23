2e926afe3b793a4e6270d8b8caed1943b9c891b7
jinhong <jinhong@LAPTOP-JINHONG>
2021-10-23 19:34:22 +0900

    추가 : DAO - MemberDao, MemberDaoImpl

---
+++ src/main/java/com/project/emosic/member/model/dao/MemberDao.java
@@ -0,0 +1,10 @@
+package com.project.emosic.member.model.dao;
+
+import com.project.emosic.member.model.vo.Member;
+
+public interface MemberDao {
+
+	int insertMember(Member member);
+
+	
+}
---
+++ src/main/java/com/project/emosic/member/model/dao/MemberDaoImpl.java
@@ -0,0 +1,22 @@
+package com.project.emosic.member.model.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.project.emosic.member.model.vo.Member;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Repository
+public class MemberDaoImpl implements MemberDao {
+
+	@Autowired
+	private SqlSession session;
+
+	@Override
+	public int insertMember(Member member) {
+		return session.insert("member.insertMember", member);
+	}
+}
