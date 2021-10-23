c16eecf9beab9117c716ccfb7809d43c930ca36a
jinhong <jinhong@LAPTOP-JINHONG>
2021-10-23 19:34:57 +0900

    추가 : Service - MemberService, MemberServiceImpl

---
+++ src/main/java/com/project/emosic/member/model/service/MemberService.java
@@ -0,0 +1,8 @@
+package com.project.emosic.member.model.service;
+
+import com.project.emosic.member.model.vo.Member;
+
+public interface MemberService {
+
+	int insertMember(Member member);
+}
---
+++ src/main/java/com/project/emosic/member/model/service/MemberServiceImpl.java
@@ -0,0 +1,24 @@
+package com.project.emosic.member.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.emosic.member.model.dao.MemberDao;
+import com.project.emosic.member.model.vo.Member;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class MemberServiceImpl implements MemberService {
+
+	@Autowired
+	private MemberDao memberDao;
+
+	@Override
+	public int insertMember(Member member) {
+		
+		return memberDao.insertMember(member);
+	}
+	
+}
